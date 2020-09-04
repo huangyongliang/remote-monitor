90d33f6a4c22a1073a7981176513b6fe18f57159
huangyl <363212682@qq.com>
2020-09-04 11:40:59 +0800

    init project

---
+++ src/main/java/com/hyl/remotemonitor/RemoteMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.hyl.remotemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyl/remotemonitor/RemoteMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyl.remotemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
